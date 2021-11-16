616d784b4a6c76ebea9d08004e0c49b9a915880c
klem <ddoherty6@gmail.com>
2021-11-16 11:37:54 -0800

    fixed player name null/blank bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,8 +5,20 @@ var randomNumber = function(min, max) {
   return value;
 }
 
+var getPlayerName = function(){
+
+  var name = "";
+
+  while (name === "" || name === null) {
+    name = window.prompt("What is your robot's name?");
+  }
+
+  console.log("Player robot name is " + name);
+  return name;
+}
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
@@ -142,29 +154,11 @@ var shop = function() {
   switch (shopOptionPrompt) {
     case "REFILL":
     case "refill":
-      if (playerInfo.money >= 7) {
-      window.alert("Refilling player's health by 20 for 7 dollars.");
-
-      //increase health and decrease money
-      playerInfo.health = playerInfo.health +20;
-      playerInfo.money = playerInfo.money -7;
-      
-      } else {
-      window.alert("You don't have enough money!");
-      }
+      playerInfo.refillHealth();
       break;
     case "UPGRADE":
     case "upgrade":
-      if (playerInfo.money >= 7) {
-      window.alert("Upgrading player's attack by 6 for 7 dollars.");
-
-      //increase attack decrease money
-
-      playerInfo.attack = playerInfo.attack + 6;
-      playerInfo.money = playerInfo.money - 7;
-      } else {
-        window.alert("You don't have enough money!");
-      }
+      playerInfo.upgradeAttack();
       break;
     case "LEAVE":
     case "leave":
